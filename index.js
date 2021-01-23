2bd90df15f3201137af12639994b52ab3e7af0dd
erikn775 <erikn775@gmail.com>
2021-01-23 10:32:09 -0700

    commit tree 4319df825c680f9e61cda1c274ecb6393f25d861

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let element = document.getElementById('main')
+element.remove()
+
+let newHeader;
+ newHeader = document.createElement('h1')
+ newHeader.id = "victory"
+ newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>";
